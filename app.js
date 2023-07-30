c54a9541b0ea63f17e7f15b147e14d1d90c2c503
harini-08-12 <112260958+harini-08-12@users.noreply.github.com>
2023-07-30 10:47:02 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".effect",{
+    strings:[
+        "YouTuber","Developer",
+        "Blogger", "Designer",
+        "Freelancer"
+    ],
+    typeSpeed: 60,
+    backSpeed: 40,
+    loop: true
+});
